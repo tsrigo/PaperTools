b345a25b0bc8fe52d141237784f17c264b8ba8eb
tsrigo <717660037@qq.com>
2025-10-11 11:00:29 +0800

    Update papers: 2025-10-10

--- update_webpage.sh
+++ update_webpage.sh
@@ -1,5 +1,9 @@
-python3 papertools.py run --mode full --skip-serve
+# 1. 运行流水线生成新数据
+/home/xuanli/miniconda3/bin/python3 papertools.py run --mode full --skip-serve
 
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519_wk
+# 2. 推送更新
 git add .
 git commit -m "Update papers: $(date -d "yesterday" +%Y-%m-%d)"
 git push origin master
