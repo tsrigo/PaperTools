7d3698d4da5624faf9d4a5ce42531e2e58286134
tsrigo <717660037@qq.com>
2025-10-21 12:05:02 +0800

    Update papers: 2025-10-21

--- update_webpage.sh
+++ update_webpage.sh
@@ -5,5 +5,5 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519_wk
 # 2. 推送更新
 git add .
-git commit -m "Update papers: $(date -d "yesterday" +%Y-%m-%d)"
+git commit -m "Update papers: $(date +%Y-%m-%d)"
 git push origin master
